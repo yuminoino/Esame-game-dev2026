36cea67656e3146064169e6399a65be10f8310f9
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-01-23 22:42:19 +0100

    loopbackground

--- Assets/scripts/RepeatBackground.cs
+++ Assets/scripts/RepeatBackground.cs
@@ -1,20 +1,36 @@
 using UnityEngine;
 
-public class MoveAndRepeatBackground : MonoBehaviour
+public class LoopBackground2D : MonoBehaviour
 {
     public float speed = 3f;
+    public Transform otherBg;
 
+    private SpriteRenderer sr;
     private float width;
+    private float halfWidth;
 
     void Start()
     {
-        width = GetComponent<SpriteRenderer>().bounds.size.x;
+        sr = GetComponent<SpriteRenderer>();
+        width = sr.bounds.size.x;
+        halfWidth = sr.bounds.extents.x; // = width/2
     }
+
     void Update()
     {
-        // Muove a sinistra
+        // movimento
         transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        // bordo sinistro della camera
+        float camLeft = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x;
+
+        // se questo pezzo è COMPLETAMENTE fuori a sinistra
+        if (sr.bounds.max.x < camLeft)
+        {
+            // mettilo subito a destra dell'altro pezzo
+            float otherRight = otherBg.GetComponent<SpriteRenderer>().bounds.max.x;
+            transform.position = new Vector3(otherRight + halfWidth, transform.position.y, transform.position.z);
+        }
     }
 }
 
-// Quando è uscito a sinistra, lo rimette in coda (serve che ci siano 2 bg affiancati)
