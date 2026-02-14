2604dc09de55d56f962e445bf2a8dee6bf9798d0
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-02-14 12:52:20 +0100

    Double jump

--- Assets/scripts/PlayerJump.cs
+++ Assets/scripts/PlayerJump.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
     public float jumpForce = 10f;
+    public int maxJumpCount = 2;  // how many jumps the player can do before touching the ground again
+
     private Rigidbody2D playerRb;
+    private int jumpCount = 0;
     private bool isOnGround = true;
 
     void Start()
@@ -13,19 +16,20 @@ public class PlayerJump : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount)
         {
             playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount++;
             isOnGround = false;
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter22D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            jumpCount = 0; // reset jump count when player touches the ground
         }
     }
 }
-
