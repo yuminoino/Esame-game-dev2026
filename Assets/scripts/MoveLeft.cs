f8fc01d1f24cd161d1fbab8ad4928c0e499006de
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-02-14 21:05:00 +0100

    Update MoveLeft.cs

--- Assets/scripts/MoveLeft.cs
+++ Assets/scripts/MoveLeft.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    private float speed = 20f;
+    public float speed = 20f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
