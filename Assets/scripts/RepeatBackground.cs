fc4598a30977bd1a6b365b984bd339615c83f89b
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-02-16 21:17:39 +0100

    Descriptions

--- Assets/scripts/RepeatBackground.cs
+++ Assets/scripts/RepeatBackground.cs
@@ -15,7 +15,7 @@ public class LoopBackground2D : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
         width = sr.bounds.size.x; // get the width of the background sprite from the SpriteRenderer's bounds, which will be used to determine when to loop the background
-        halfWidth = sr.bounds.extents.x; // = width/2
+        halfWidth = sr.bounds.extents.x; // = width/2 - get half of the width of the background sprite from the SpriteRenderer's bounds, which will be used to position the background correctly when looping
     }
 
     // Update is called once per frame
@@ -29,14 +29,14 @@ public class LoopBackground2D : MonoBehaviour
         // Move left
         transform.Translate(Vector3.left * speed * Time.deltaTime);
 
-        // sx 
+        // Get the x coordinate of the left edge of the screen in world space, which will be used to determine when to loop the background
         float camLeft = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x;
 
         // if completely out of screen on the left
         if (sr.bounds.max.x < camLeft)
         {
             // put it to the right of the other background
-            float otherRight = otherBg.GetComponent<SpriteRenderer>().bounds.max.x;
+            float otherRight = otherBg.GetComponent<SpriteRenderer>().bounds.max.x; // get the x coordinate of the right edge of the other background sprite from its SpriteRenderer's bounds, which will be used to position this background correctly when looping
             transform.position = new Vector3(otherRight + halfWidth, transform.position.y, transform.position.z);
         }
     }
